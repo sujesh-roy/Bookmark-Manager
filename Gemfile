ba452c52a0da5511402807a342f89c159595deb3
Rahul Shah <rahulshah7@outlook.com>
2019-11-25 15:43:39 +0000

    Set up Sinatra with RSpec and Capybara

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+
+group :development, :test do
+  gem 'byebug', require: false
+  gem 'capybara'
+  gem 'guard-rspec', require: false
+  gem 'rack'
+  gem 'rspec', require: false
+  gem 'rubocop', require: false
+  gem 'ruby-debug-ide', require: false
+  gem 'selenium-webdriver'
+  gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
+  gem 'webdrivers'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+require 'simplecov-console'
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+ENV['RACK_ENV'] = 'test'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new [SimpleCov::Formatter::Console]
+
+SimpleCov.start
+
+# For accurate test coverage measurements, require your code AFTER 'SimpleCov.start'
+
+require_relative './features/web_helper.rb'
+require_relative '../app'
+Capybara.app = BookmarkManager
